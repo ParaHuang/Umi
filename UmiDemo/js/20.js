70dfe45da3493c94a8ee01e2827bbf015435c15d
ParaHuang <paradox041@gmail.com>
2025-06-28 16:37:07 +0800

    node

---
+++ UmiDemo/js/20.js
@@ -0,0 +1,15 @@
+let number = document.getElementById("number");
+let name = document.getElementById("name");
+let age = document.getElementById("age");
+let cls = document.getElementById("class");
+
+
+let add = function(){
+    let numberStr = number.value;
+    let nameStr = name.value;
+    let ageStr = age.value;
+    let clsStr = cls.value;
+    
+    
+    
+}
