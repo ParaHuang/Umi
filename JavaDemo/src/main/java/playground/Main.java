664f19818ef01bd2d3ebfc6321d70226e9ac1309
Ümit <umit.ceyran@protonmail.ch>
2025-08-17 16:05:31 +0200

    homework

--- JavaDemo/src/main/java/playground/Main.java
+++ JavaDemo/src/main/java/playground/Main.java
@@ -1,4 +1,16 @@
 package playground;
 
 public class Main {
+    public static void main(String[] args) {
+        // Creating Objects
+        behindthescenes.Date date1 = new behindthescenes.Date(2024, 12, 25);
+        behindthescenes.Date date2 = new behindthescenes.Date(2024, 12);
+        behindthescenes.Date date3 = new behindthescenes.Date(2024);
+        behindthescenes.Date date4 = new behindthescenes.Date();
+
+        System.out.println(date1);
+        System.out.println(date2);
+        System.out.println(date3);
+        System.out.println(date4);
+    }
 }
