5d401d43841fb8371a8eb820ad88bf89f71a10b3
Ümit Can Ceyran <umit.ceyran.admin@proton.me>
2025-09-21 15:56:35 +0200

    Solved

--- JavaDemo/src/main/java/playground/comprehensive/Main.java
+++ JavaDemo/src/main/java/playground/comprehensive/Main.java
@@ -28,10 +28,15 @@ public class Main {
                 case 1:
                     System.out.println("Please enter every information of student");
                     // TODO: Einzelne Beschreibungen zum Inout hinzufügen
+                    System.out.println("ID:");
                     int id = input.nextInt();
+                    System.out.println("Name:");
                     String name = input.next();
+                    System.out.println("Age:");
                     int age  = input.nextInt();
+                    System.out.println("Gender:");
                     String gender  = input.next();
+                    System.out.println("height:");
                     int height  = input.nextInt();
 
                     Student student = new Student(id, name, age, gender, height);
@@ -41,7 +46,7 @@ public class Main {
                     for (Student s : students) {
                         if (s.getId() == id) {
                             exists = true;
-                            System.out.println("Student ID: " + s.getId() + "is already in the System");
+                            System.out.println("Student ID: " + s.getId() + " is already in the System");
                             break;
                         }
 
@@ -56,10 +61,47 @@ public class Main {
                     System.out.println(students);
                     break;
                 case 3:
+                    System.out.println("Pls enter a Students ID");
+
+                    int searchID = input.nextInt();
+                    boolean existID = false;
+
+                    for (Student s : students) {
+                        if (s.getId() == searchID) {
+                            existID = true;
+                            System.out.println("Student ID: " + s.getId() + "it exists in the System");
+                            break;
+                        }
+
+
+                    }
+                    if (!existID) {
+                        System.out.println("Student ID: " + searchID + " does not exist in the System");
+                    }
                     break;
                 case 4:
+                    System.out.println("Pls enter a Students ID");
+
+                    int deleteID = input.nextInt();
+                    boolean dexistID = false;
+
+                    for (Student s : students) {
+                        if (s.getId() == deleteID) {
+                            dexistID = true;
+                            //System.out.println("Student ID: " + s.getId() + "it exists in the System");
+                            students.remove(s);
+                            break;
+
+                        }
+
+
+                    }
+                    if (!dexistID) {
+                        System.out.println("Student ID: " + deleteID + " does not exist in the System");
+                    }
                     break;
                 default:
+                    System.out.println("Thank you for using this program");
                     return;
             }
 
